9eb26ffcb2ec0f861dc9ab12e16abbf76668b116
Lalit <125788619+lalitcap23@users.noreply.github.com>
2024-07-31 23:11:52 +0530

    it is the feature of the connect username and bio with the string and address

---
+++ intrface.sol
@@ -0,0 +1,23 @@
+
+// SPDX-License-Identifier: MIT
+// Software Package Data Exchange
+
+pragma solidity ^0.8.0;
+
+contract Profile {
+    struct UserProfile {
+        string displayName;
+        string bio;
+    }
+    
+    mapping(address => UserProfile) public profiles;
+
+    function setProfile(string memory _displayName, string memory _bio) public {
+        
+        profiles[msg.sender] = UserProfile(_displayName, _bio);
+    }
+
+    function getProfile(address _user) public view returns (UserProfile memory) {
+        return profiles[_user];
+    }
+}
